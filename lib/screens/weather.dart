edea0d3f6553ea0556ae9a072e6d72ede982b2f6
kriteshshrestha-62 <kriteshshrestha2062@gmail.com>
2026-03-30 23:09:19 +0545

    Weather Model Change

---
+++ lib/screens/weather.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Weather extends StatelessWidget {
+  const Weather({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
